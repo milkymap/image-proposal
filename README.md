437287dea8bc5460a99044c07dbb5dc061813147
milkymap <ibrahima.elmokhtar@gmail.com>
2023-09-12 14:05:47 +0200

    update : adapt Dockerfile and add README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+# text-semantic-embedding
+
+# build image 
+
+```bash
+docker build -t image-proposal:gpu -f Dockerfile.gpu .
+```
+
+# run container 
+```bash
+    docker run --rm --name image-proposal -gpus all \
+        -v /path/to/transformers_cache/:/home/solver/transformers_cache \
+        -v /path/to/base_dir/:/home/solver/base_dir \
+        -e ES_HOST="elastic search hostname" \
+        -e ES_PORT="elastic search port" \
+        -e ES_SCHEMA=https
+        -e ES_BASINC_AUTH=XXXXXXX \
+        -p 8000:8000 \
+        image-proposal:gpu \ 
+            --port 8000 
+            --host '0.0.0.0' 
+            --img_model_name clip-ViT-L-14
+            --nlp_model_name Sahajtomar/french_semantic 
+            --chunk_size 128 
+            --nb_nlp_workers 1 
+            --nb_img_workers 2 
+            --mounting_path "/"
+```
+
--- main.py
+++ main.py
@@ -11,20 +11,21 @@ from time import sleep
 
 """
 python main.py --nlp_model_name Sahajtomar/french_semantic --img_model_name clip-ViT-L-14 --chunk_size 128 --timeout 1000 --nb_nlp_workers 1 --nb_img_workers 1
+
 """
+
 import multiprocessing as mp 
 from starter import launch_runner, launch_server
 
 @click.command()
 @click.option('--host', default='0.0.0.0')
 @click.option('--port', default=8000)
-@click.option('--path2base_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False), required=True)
+@click.option('--path2base_dir', envvar='PATH2BASE_DIR', type=click.Path(exists=True, dir_okay=True, file_okay=False), required=True)
 
 @click.option('--es_host', type=str, envvar='ES_HOST', required=True)
 @click.option('--es_port', type=int, envvar='ES_PORT', required=True)
 @click.option('--es_scheme', type=str, envvar='ES_SCHEME', required=True)
 @click.option('--es_basic_auth', type=str, envvar='ES_BASIC_AUTH', required=True)
-@click.option('--es_index_name', type=str, required=True)
 @click.option('--path2index_schema', type=click.Path(exists=True, dir_okay=False, file_okay=True), default='es_schema.json')
 
 @click.option('--nlp_model_name', required=True)
@@ -36,8 +37,9 @@ from starter import launch_runner, launch_server
 @click.option('--protocol_buffers', envvar='PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION', required=True, type=click.Choice(['python']))
 def main(host:str, port:int, path2base_dir:str, es_host:str, es_port:int, es_scheme:str, es_basic_auth:str, path2index_schema:str ,nlp_model_name:str, img_model_name:str, nb_nlp_workers:int, nb_img_workers:int ,chunk_size:int, cache_folder:str, protocol_buffers:str):
     device = 'cuda:0' if th.cuda.is_available() else 'cpu'
-    th.multiprocessing.set_start_method('spawn')
-    logger.info('multiprocessing => start_method set to spawn for cuda')
+    if device == 'cuda:0':
+        th.multiprocessing.set_start_method('spawn')
+        logger.info('multiprocessing => start_method set to spawn for cuda')
 
     runner_process = mp.Process(
         target=launch_runner,
