6a40deffa60f3e6bd5799689cb69c2525995366c
milkymap <ibrahima.elmokhtar@gmail.com>
2023-09-13 12:01:23 +0200

    spawn to fork

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ import multiprocessing as mp
 from server import APIServer
 from services.broker import ZMQBroker
 from services.worker import ZMQWorker
-from vectorizer import NLPVectorizer, IMGVectorizer
+
 
 from typing import List, Dict, Tuple, Any, Type
 
@@ -45,6 +45,17 @@ class ZMQRunner:
         switch_id = f'switch_{nb_services:03d}'
         self.topics_acc.append(topics)
         self.services.append((name, builder, switch_id, kwargs, nb_workers))
+    
+    def start_server(self, server_config:Dict[str, str]):
+        server_process = mp.Process(
+            target=self.launch_server,
+            kwargs=server_config
+        )
+        self.processes.append(
+            server_process
+        )
+
+        self.processes[-1].start()
 
     def loop(self):
         broker_ = mp.Process(target=self.launch_broker)
@@ -89,9 +100,18 @@ class ZMQRunner:
         with builder(**kwargs) as worker:
             worker.before_loop(worker_id, switch_id)
             worker.loop()
+    
+    def launch_server(self, host:str, port:int, mounting_path:str ,path2base_dir:str, es_host:str, es_port:int, es_scheme:str, es_basic_auth:str, path2index_schema:str):
+        async def start_server(host:str, port:int, path2base_dir:str, mounting_path:str):
+            async with APIServer(host, port, path2base_dir, number_opened_file_limit=8192, mounting_path=mounting_path) as server:
+                await server.loop(es_host, es_port, es_scheme, es_basic_auth, path2index_schema)
+        
+        asyncio.run(start_server(host, port, path2base_dir, mounting_path))
+
 
     def __enter__(self):
         self.__initialize_signal_handler()
+    
         return self 
     
     def __exit__(self, exc_type, exc_value, traceback):
